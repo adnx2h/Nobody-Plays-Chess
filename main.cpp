178556eaa7da2e78de7eb3dfd9bff9ccaf6ae320
Adan Gonzalez <adnx2h@gmail.com>
2021-01-22 14:35:28 -0500

    testing ssh login

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ using namespace std;
 int main() {
     cout << "Hello World!" << endl;
     Board myBoard;
+    myBoard.printEmptyBoard();
     myBoard.getPiecesFromFEN(FEN_START);
     myBoard.printBoardTest();
   return 0;
